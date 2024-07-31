85db53ba8173b746cc5fd694bb7dbae563bf32c9
4z0t <gastec139@gmail.com>
2024-07-31 23:23:32 +0300

    wip

--- COFFData.py
+++ COFFData.py
@@ -20,6 +20,40 @@ class COFFSect:
                         offset=offset)
 
 
-class COFFData:
+class COFFData(BasicBinaryParser):
     def __init__(self, file_path: str) -> None:
-        pass
+        data: bytes = None
+        self.name: str = file_path
+        self.sects: list[COFFSect] = []
+        with open(file_path, "rb") as f:
+            data = f.read()
+
+        if data is None:
+            raise Exception(f"Couldn't read data from file {file_path}")
+
+        super().__init__(data)
+
+        self.parse_sects_data()
+
+    def parse_sects_data(self):
+        self.pos = 2
+        sect_count = self.value("H")
+        self.pos = 20
+        for i in range(sect_count):
+            name = self.value("8s").decode().replace("\x00", "")
+            if name[0] == "h":
+                self.sects.append(COFFSect(name, 0, 0))
+            self.pos += 0x20
+
+        self.pos = 8
+        pos, count = self.values("II")
+        self.pos = pos
+
+        for i in range(count):
+            pass
+
+    def find_sect(self, name) -> Optional[COFFSect]:
+        for sect in self.sects:
+            if sect.name == name:
+                return sect
+        return None
