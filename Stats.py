0ea93a4c001b3375ff362762fc79c0ad387a7fee
4z0t <gastec139@gmail.com>
2024-10-11 19:47:52 +0300

    Update Stats.py

--- Stats.py
+++ Stats.py
@@ -9,24 +9,13 @@ class Function:
     type: str
 
 
-functions: list[Function] = []
-
 TYPE_TO_NAME = {
     "O": "Original",
     "U": "Unused",
     "R": "Replicated",
-    "T": "To Be Replicated"
+    "T": "To Be Replicated",
+    "X": "Unmarked"
 }
-
-with open("stats.txt", "r") as f:
-    for line in f.readlines():
-        if line.startswith("#") or not line.strip():
-            continue
-        start, end, type, name = line.split(" ", 3)
-        start = int(start, 16)
-        end = int(end, 16)
-        functions.append(
-            Function(start, end, name, TYPE_TO_NAME[type.upper()]))
 
 TEXT_START = 0x00401000
 TEXT_END = 0x00C0F000
@@ -41,10 +30,47 @@ def compute_total(type):
     return total
 
 
-ratio = (compute_total("Replicated") + compute_total("Original")) / \
-    (TEXT_END-TEXT_START-compute_total("Unused"))
-print(f"Replicated: {ratio*100:.3f}%")
-print(f"Unused functions bytes:\t\t{compute_total("Unused")}")
-print(f"Replicated functions bytes:\t{compute_total("Replicated")}")
-print(f"Original functions bytes:\t{compute_total("Original")}")
-print(f"To be replicated:\t\t{compute_total("To Be Replicated")}")
+def read_file_functions_data(file_path) -> list[Function]:
+    functions = []
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
+            start, end, type, name = line.split(" ", 3)
+            start = int(start, 16)
+            end = int(end, 16)
+            functions.append(
+                Function(start, end, name, type.upper()))
+    return functions
+
+
+def write_file_functions_data(file_path, functions: list[Function]):
+    with open(file_path, "w") as f:
+        for fn in functions:
+            f.write(f"{fn.start:08x} {fn.end:08x} {fn.type} {fn.name}\n")
+    return functions
+
+
+def merge_stats(file_dest, file_source):
+    initial = read_file_functions_data(file_dest)
+
+    def find_by_start(start):
+        for i, fn in enumerate(initial):
+            if fn.start == start:
+                return i
+        return None
+
+    new_functions = read_file_functions_data(file_source)
+    for fn in new_functions:
+        i = find_by_start(fn.start)
+        if i is not None:
+            initial.pop(i)
+
+    initial.extend(new_functions)
+
+    initial.sort(key=lambda fn: fn.start)
+    write_file_functions_data(file_dest, initial)
+
+
+merge_stats("stats2.txt", "stats.txt")
