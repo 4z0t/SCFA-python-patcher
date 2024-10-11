429ed340480c5800eca6d7243a3f5bd0c200db40
4z0t <gastec139@gmail.com>
2024-10-11 23:05:16 +0300

    wip stats

--- Stats.py
+++ Stats.py
@@ -14,20 +14,12 @@ TYPE_TO_NAME = {
     "U": "Unused",
     "R": "Replicated",
     "T": "To Be Replicated",
-    "X": "Unmarked"
+    "X": "Unmarked",
+    "N": "Not Done"
 }
 
 TEXT_START = 0x00401000
 TEXT_END = 0x00C0F000
-
-
-def compute_total(type):
-    global functions
-    total = 0
-    for func in functions:
-        if func.type == type:
-            total += func.end - func.start
-    return total
 
 
 def read_file_functions_data(file_path) -> list[Function]:
@@ -44,6 +36,8 @@ def read_file_functions_data(file_path) -> list[Function]:
                 fn.end = start
             fn = Function(start, 0, name, type.upper())
             functions.append(fn)
+    if fn is not None:
+        fn.end = TEXT_END
     return functions
 
 
@@ -56,10 +50,9 @@ def read_file_functions_data_with_end(file_path) -> list[Function]:
                 continue
             start, end, type, name = line.split(" ", 3)
             start = int(start, 16)
+            end = int(end, 16)
             fn = Function(start, end, name, type.upper())
             functions.append(fn)
-    if fn is not None:
-        fn.end = TEXT_END
     return functions
 
 
@@ -70,7 +63,7 @@ def write_file_functions_data(file_path, functions: list[Function]):
     return functions
 
 
-def merge_stats(file_dest, file_source):
+def merge_stats(file_dest, file_source) -> list[Function]:
     initial = read_file_functions_data(file_dest)
 
     def find_by_start(start):
@@ -89,6 +82,24 @@ def merge_stats(file_dest, file_source):
 
     initial.sort(key=lambda fn: fn.start)
     write_file_functions_data(file_dest, initial)
+    return initial
 
 
-merge_stats("stats3.txt", "stats.txt")
+functions = merge_stats("stats3.txt", "stats.txt")
+
+
+def compute_total(functions, type):
+    total = 0
+    for func in functions:
+        if func.type == type:
+            total += func.end - func.start
+    return total
+
+
+ratio = (compute_total(functions, "R") + compute_total(functions, "O")) / \
+    (TEXT_END-TEXT_START-compute_total(functions, "U"))
+print(f"Replicated: {ratio*100:.3f}%")
+print(f"Unused functions bytes:\t\t{compute_total(functions, "U")}")
+print(f"Replicated functions bytes:\t{compute_total(functions, "R")}")
+print(f"Original functions bytes:\t{compute_total(functions, "O")}")
+print(f"To be replicated:\t\t{compute_total(functions, "T")}")
